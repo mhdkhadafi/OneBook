fdcd2129548f1415ecd00943c7cec31fae2b3fd8
Brandon <baplaste@gmail.com>
2015-10-24 21:04:52 -0400

    MediaRetriever added

---
+++ Assets/Scripts/MediaRetriever.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class MediaRetriever : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Starting Media Retriever");
+		currentTitleID = 10;
+		currentMediaType = 0;
+		populateTitles();
+
+		if(currentMediaType == 0)
+			getMostRecentArticle(currentTitleID);
+		else if(currentMediaType == 1)
+			incrementPage();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	private string[][] titles;
+	private int currentMediaType;
+	private string[] mediaTypes;
+	private int currentTitleID;
+	private int currentArticleID = 0;
+	private string currentArticleText;
+	private int currentPage = 0;
+	private string currentPageText;
+	private int linesPerPage = 100;
+	private string apiKey = "";
+	private WWW currentArticle;
+
+	private void populateTitles(){
+		mediaTypes = new string[]{"magazines", "books"};
+		string[] magazines = new string[]{"cosmopolitan", 
+		                   "countryliving",
+		                   "delish",
+		                   "elle",
+		                   "elledecor",
+		                   "esquire",
+		                   "goodhousekeeping",
+		                   "harpersbizarre",
+		                   "housebeautiful",
+		                   "marieclaire",
+		                   "popularmechanics",
+		                   "redbookmag",
+		                   "roadandtrack",
+		                   "seventeen",
+		                   "townandcountrymag",
+		                   "veranda",
+		                   "womansday"
+							};
+
+		string[] books = new string[]{"Frankenstein", 
+										"Ulysses"};
+
+		titles = new string[][]{magazines, books};
+	}
+
+	public void incrementMediaType() {
+		currentMediaType = (currentMediaType + 1)%titles.Length;
+	}
+	
+	public void decrementMediaType() {
+		currentMediaType = (currentMediaType - 1)%titles.Length;
+	}
+
+	public string getCurrentMediaType(){
+		return mediaTypes[currentMediaType];
+	}
+	
+	public void incrementTitle() {
+		currentTitleID = (currentTitleID + 1)%titles[currentMediaType].Length;
+	}
+
+	public void decrementTitle() {
+		currentTitleID = (currentTitleID - 1)%titles[currentMediaType].Length;
+	}
+
+	public string getCurrentTitle() {
+		return titles[currentMediaType][currentTitleID];
+	}
+
+	public void incrementArticle() {
+		if(currentArticleID > 0) currentArticleID--;
+		currentArticle = getArticle(currentArticleID, currentTitleID);
+	}
+
+	public void decrementArticle() {
+		currentArticleID++;
+		currentArticle = getArticle(currentArticleID, currentTitleID);
+	}
+
+	private WWW getArticle(int articleID, int titleID){
+		string title = titles[currentMediaType][titleID];
+		string url = "https://" + title + ".hearst.io/api/v1/articles?id=" + articleID.ToString() + "&all_images=0&get_image_cuts=0&ignore_cache=0&_key=" + apiKey;
+		return GET (url, generateTexture);
+	}
+
+	private void getMostRecentArticle(int titleID){
+		string title = titles[currentMediaType][titleID];
+		string url = "https://" + title + ".hearst.io/api/v1/articles?visibility=1&all_images=0&get_image_cuts=0&ignore_cache=0&limit=1&order_by=date+desc&_key=" + apiKey;
+		currentArticle = GET (url, generateTexture);
+	}
+
+	private void generateTexture(){
+		Debug.Log("article received");
+		var js = JSON.Parse(currentArticle.text);
+		var item = js["items"][0];
+		if(int.TryParse(item["id"], out currentArticleID)){
+			currentArticleText = item["body"];
+			Debug.Log(currentArticleText);
+		} else {
+			Debug.LogError("Failed to Parse Article ID");
+		}
+	}
+
+	public void incrementPage(){
+		currentPage++;
+		currentPageText = getPage(currentPage, currentTitleID);
+	}
+
+	public void decrementPage(){
+		if(currentPage > 0) currentPage--;
+		currentPageText = getPage(currentPage, currentTitleID);
+	}
+	
+	private string getPage(int page, int titleID){
+		int minLine = (page - 1)*linesPerPage;
+		int maxLine = minLine + linesPerPage;
+		string text = "";
+
+		using (var reader = new System.IO.StreamReader("Assets/Books/" + titles[currentMediaType][titleID] + ".txt"))
+		{
+			int lineCount = 0;
+
+			while (!reader.EndOfStream && lineCount <= maxLine)
+			{
+				var line = reader.ReadLine();
+				if (string.IsNullOrEmpty(line))
+					continue;
+				
+				if (lineCount > minLine)
+					text += line;
+
+				lineCount++;
+			}
+		}
+
+		return text;
+	}
+	
+	public string getCurrentPage(){
+		return currentPageText;
+	}
+	
+	
+//	void OnGUI(){
+//		GUIStyle style = new GUIStyle();
+//		style.richText = true;
+//		GUILayout.Label(currentPageText);
+//	}
+
+	private WWW GET(string url, System.Action onComplete ) {
+		
+		WWW www = new WWW (url);
+		StartCoroutine (WaitForRequest (www, onComplete));
+		return www;
+	}
+	
+//	private WWW POST(string url, Dictionary<string,string> post, System.Action onComplete) {
+//		WWWForm form = new WWWForm();
+//		
+//		foreach(KeyValuePair<string,string> post_arg in post) {
+//			form.AddField(post_arg.Key, post_arg.Value);
+//		}
+//		
+//		WWW www = new WWW(url, form);
+//		
+//		StartCoroutine(WaitForRequest(www, onComplete));
+//		return www;
+//	}
+	
+	private IEnumerator WaitForRequest(WWW www, System.Action onComplete) {
+		yield return www;
+		// check for errors
+		if (www.error == null) {
+			onComplete();
+		} else {
+			Debug.Log (www.error);
+		}
+	}
+}
