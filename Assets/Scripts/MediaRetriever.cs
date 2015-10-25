daf54b867f6b668a16c13428bf18a0ec821472a4
alapparikh <alap18@gmail.com>
2015-10-25 01:31:10 -0400

    book cover changes now

--- Assets/Scripts/MediaRetriever.cs
+++ Assets/Scripts/MediaRetriever.cs
@@ -7,8 +7,8 @@ public class MediaRetriever : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Starting Media Retriever");
-		apiKey = System.IO.File.ReadAllText("Assets/Resources/hearstSecret.txt");
-		Debug.Log(apiKey);
+//		apiKey = System.IO.File.ReadAllText("Assets/Resources/hearstSecret.txt");
+//		Debug.Log(apiKey);
 		currentTitleID = 10;
 		currentMediaType = 0;
 		populateTitles();
@@ -31,7 +31,7 @@ public class MediaRetriever : MonoBehaviour {
 	private int currentPage = 0;
 	private string currentPageText;
 	private int linesPerPage = 50;
-	private string apiKey = "";
+	private string apiKey = "2dyz29j4caczfam4y3bd5cpc";
 	private WWW currentArticle;
 
 	enum MediaTypes {Magazine, Book};
--- Assets/Scripts/VirtualButtonEventHandler.cs
+++ Assets/Scripts/VirtualButtonEventHandler.cs
@@ -5,9 +5,11 @@ using Vuforia;
 public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler {
 
 	TextMesh tm;
+	public Material[] m_materials;
 
 	// Use this for initialization
 	void Start () {
+
 		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
 		for (int i=0; i < vbs.Length; ++i) {
 			vbs[i].RegisterEventHandler(this);
@@ -16,17 +18,54 @@ public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandl
 	
 	// Update is called once per frame
 	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
-		tm = transform.Find("PageText").GetComponent<TextMesh>();
-		BookText ip = transform.Find("PageText").GetComponent<BookText>();
+
 		switch (vb.VirtualButtonName) {
 			case "changePage":
+			tm = transform.Find("PageText").GetComponent<TextMesh>();
+			BookText ip = transform.Find("PageText").GetComponent<BookText>();
+
 			ip.changeBook();
 			break;
+			case "changeBook":
+				Texture2D p = new Texture2D(10, 10);
+				Debug.Log ("OnButtonPressed called");
+				
+				p = Resources.Load("CatsCradleBook.Front", typeof(Texture2D)) as Texture2D;
+				//		Plane plane = transform.Find ("Front").GetComponent<Plane>();
+				GameObject mt = GameObject.Find("MultiTarget");
+				Debug.Log(mt);
+				Transform go = mt.transform.Find("Front");
+				if(go!=null){
+//					Material mat = go.GetComponent<Material> ();
+//					mat.mainTexture = p;
+					foreach(Component c in go.GetComponents<Component>()){
+						Debug.Log(c);
+					}
+//					Material m = Resources.Load("CatsCradleBook.Front.mat", typeof(Material)) as Material;
+					Renderer r = go.GetComponent<MeshRenderer>();
+					r.material = m_materials[0];
+//					r.material =m;
+				}
+				break;
 		}
 
 	
 	}
 
 	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb) {
+		switch (vb.VirtualButtonName) {
+		case "changePage":
+			break;
+		case "changeBook":
+//			Texture2D p = new Texture2D(10, 10);
+//			p = Resources.Load("CatsCradleBook.Back", typeof(Texture2D)) as Texture2D;
+//			Transform go = transform.Find("MultiTarget").Find("Front");
+//			if(go!=null){
+//				Material mat = go.GetComponent<Material> ();
+//				mat.mainTexture = p;
+//			}			
+//			Debug.Log ("OnButtonReleased called");
+			break;
+		}
 	}
 }
